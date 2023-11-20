aec8777341c722af245b15c039777ebb02050537
Aleksandr Filatov <greybax@gmail.com>
2023-11-20 12:19:16 -0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const main = () => {
 
   const func = () => {
     const searchString = randomText();
-    iframe.src = `https://www.bing.com/search?q=${searchString}`;
+    iframe.src = `https://www.bing.com/search?q=${searchString}&PC=U316&FORM=CHROMN`;
     counter++;
     console.log('counter', counter);
     // browser
